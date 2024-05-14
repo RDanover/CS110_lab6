239ca1999f243f182cbfeef3559ac5579d92c927
Rachel Danover <racheldanover@Rachels-Air.fios-router.home>
2024-05-13 19:45:17 -0700

    starter code added

---
+++ book-api.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post ('/book', (req, res) => {
+    const book = req.body; 
+
+    console.log(book);
+    books.push(book) ;
+
+    res.send('Book is added to the database');
+});
+
+app.post ('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn; 
+    const newBook = req.body;
+
+    //remove item from the books array
+
+    for(let i =0; i<books.length;i++){
+        let book = books[i];
+
+        if(book.isbn === isbn){
+            books[i] = newBook;
+        }
+    }
+
+    res.send('Book is edited');
+});
+
+app.get('/books', (req,res) =>{
+    res.json(books);
+});
+
+app.listen(port, () => console.log(`Hello World app listening on port ${port}!`))
